88a79c83f44c4fd876e9709c5ebd98a9db0e5653
Son Nguyen <sonnguyen@goappable.com>
2018-03-01 17:23:39 +0700

    update api create, search and view list

--- src/sharing-lists/Popup.jsx
+++ src/sharing-lists/Popup.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Prompt} from 'nti-web-commons';
+import {Prompt, Avatar} from 'nti-web-commons';
+
+import {getStore} from '../Api';
+import {USERS} from '../Constants';
 
 export default class SharingPopup extends React.Component {
 	static show (data) {
@@ -20,24 +23,95 @@ export default class SharingPopup extends React.Component {
 		data: PropTypes.object
 	}
 
-
 	constructor (props) {
 		super(props);
 
 		this.state = {
-			userName: '',
-			members: []
+			search: '',
+			members: [],
+			searchResults: [],
+			listName: ''
 		};
 	}
 
-	changeName =(e) =>{
-		this.setState ({userName: e.target.value});
+	componentDidMount () {
+		this.setUpStore();
+	}
+
+	componentWillReceiveProps () {
+		this.setUpStore();
+	}
+
+	setUpStore = () => {
+		getStore(USERS)
+			.then(store => this.setState({store}));
+	}
+
+	attachInputRef = x => this.query = x
+
+	listNameChange = (e) => {
+		this.setState({listName: e.target.value});
+	}
+
+	queryChanged = (event) => {
+		let query = event ? event.target.value : '';
+		let {store} = this.state;
+
+		let existing = this.state.search;
+		if(existing === query) {
+			return;
+		}
+
+		this.setState({
+			search: query,
+			searchResults: []
+		});
+
+		if (store && store.search) {
+			store.search(query)
+				.then(results => {
+					let contacts = [];
+					for(let user of store) {
+						if (store.entityMatchesQuery(user, query)) {
+							contacts.push(user);
+						}
+					}
+					let users = results.filter((entity) => entity.isUser);
+					this.setState({
+						searchResults: users,
+						searchLoading: false,
+						contactsResults: contacts
+					});
+				})
+				.catch(reason=> {
+					if (typeof reason !== 'object' || reason.statusCode !== -1) {
+						this.setState({
+							searchError: reason
+						});
+					}
+				});
+		}
 	}
 
 	addPeople = (user) => () => {
 		let members = this.state.members;
-		members.push({user: user, remove: true});
-		this.setState({members: members, userName: ''});
+		let checkUserExisted = false;
+		members.map((item) => {
+			if (user.ID === item.ID) {
+				checkUserExisted = true;
+				return;
+			}
+		});
+
+		if (!checkUserExisted) {
+			members.push(user);
+		}
+
+		this.setState({
+			search: '',
+			searchResults: [],
+			members: members
+		});
 	}
 
 	remove = (pos) => () => {
@@ -54,14 +128,27 @@ export default class SharingPopup extends React.Component {
 		}
 	}
 
+	create = () => {
+		const {store, listName, members} = this.state;
+
+		if (!store) {
+			return;
+		}
+
+		store.createList(listName, members)
+			.then((result) => {
+				console.log(result);
+			});
+	}
+
 	done = () =>{
 		this.cancel();
 	}
 
 	render () {
-		const {userName, members} = this.state;
+		const {search, members, searchResults, listName} = this.state;
+
 		const {data} = this.props;
-		const people = ['user 1', 'user 2', 'user 3', 'user 4', 'user 5'];
 
 		return (
 			<div className="modal-common-sharing-group">
@@ -71,7 +158,7 @@ export default class SharingPopup extends React.Component {
 							<h2 className="title-header create-sharing-title">Create a Sharing List</h2>
 						)}
 						{!data.isCreate && (
-							<h2 className="title-header create-sharing-title">Friends <span>({data.totalFriends})</span></h2>
+							<h2 className="title-header create-sharing-title">Friends <span>({data.list.friends.length})</span></h2>
 						)}
 					</div>
 					<div className="modal-content-create-sharing">
@@ -79,7 +166,7 @@ export default class SharingPopup extends React.Component {
 							{data.isCreate && (
 								<div>
 									<label htmlFor="fname">List Name</label>
-									<input type="text" id="fname" name="firstname" placeholder="Name"/>
+									<input type="text" id="fname" name="firstname" placeholder="Name" value={listName} onChange={this.listNameChange}/>
 								</div>
 							)}
 							<label htmlFor="lname">Add People</label>
@@ -87,15 +174,19 @@ export default class SharingPopup extends React.Component {
 								<span className="notify"><i className="icon-hide icon"/>Lists are private to you. We do not notify people you add to your lists.</span>
 							)}
 							<div className="search-add-people">
-								<input type="text" id="enterName" name="Enter a name" placeholder="Enter a Name" value={this.state.userName} onChange={this.changeName}/>
-								{userName.length > 0 && (
+								<input type="text" className="search-input" ref={this.attachInputRef}
+									onChange={this.queryChanged} placeholder="Enter the name"
+								/>
+								{search.length > 0 && (
 									<div className="result-search">
 										<ul>
-											{people.map((item, index) =>{
+											{searchResults.map((item, index) =>{
 												return(
 													<li className="search-item" key={index} onClick={this.addPeople(item)}>
-														<img className="img-avatar" src="https://www.w3schools.com/howto/img_fjords.jpg"/>
-														<p className="search-name">{item}</p>
+														<div className="img-avatar">
+															<Avatar className="img-user" entityId={item.Username}/>
+														</div>
+														<p className="search-name">{item.realname}</p>
 													</li>
 												);
 											})}
@@ -103,7 +194,7 @@ export default class SharingPopup extends React.Component {
 									</div>
 								)}
 							</div>
-							{userName.length === 0 && data.isCreate && (
+							{search.length === 0 && data.isCreate && (
 								<p className="suggest hidden">Suggested <a>Harold Newman,</a> <a>Miguel
 									Wolfe,</a> <a>Lela
 									Chapman, Andy Rogers,</a>
@@ -117,12 +208,15 @@ export default class SharingPopup extends React.Component {
 												return(
 													<li className="search-item" key={index}>
 														<div className="item-left">
-															<img className="img-avatar" src="https://www.w3schools.com/howto/img_fjords.jpg"/>
-															<p className="search-name">{item.user}</p></div>
-														{item.remove && (
+															<div className="img-avatar">
+																<Avatar className="img-user" entityId={item.Username}/>
+															</div>
+															<p className="search-name">{item.realname}</p>
+														</div>
+														{!item.remove && (
 															<a className="btn-remove" onClick={this.remove(index)}>Remove</a>
 														)}
-														{!item.remove && (
+														{item.remove && (
 															<a className="btn-undo" onClick={this.remove(index)}>Undo</a>
 														)}
 													</li>
@@ -137,7 +231,7 @@ export default class SharingPopup extends React.Component {
 					<ul className="modal-button-feature">
 						{data.isCreate && (
 							<div>
-								<li><a className="btn-create">Create</a></li>
+								<li><a className="btn-create" onClick={this.create}>Create</a></li>
 								<li><a className="btn-cancel" onClick={this.cancel}>Cancel</a></li>
 							</div>
 						)}
