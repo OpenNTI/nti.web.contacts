3ba9d7d23a6a6805937a857977fa64b9c53d5ba9
Zachary Roux <zachary.roux@nextthought.com>
2018-04-04 11:25:34 -0500

    Add store and initial draft of view for sharing lists

---
+++ src/sharing-lists/Store.js
@@ -0,0 +1,39 @@
+import {Stores} from 'nti-lib-store';
+import {getService} from 'nti-web-client';
+
+export default class SharingListStore extends Stores.SimpleStore {
+
+	constructor () {
+		super();
+		this.setupDataSource();
+	}
+
+	get (key) {
+		if (key === 'loading' || key === 'error') {
+			return this.ds[key] || super.get(key);
+		}
+
+		if (key === 'items') {
+			return Array.from(this.ds);
+		}
+
+		return super.get(key);
+	}
+
+	async setupDataSource () {
+		try {
+			const service = await getService();
+			const ds = this.ds = service.getLists();
+
+			ds.addListener('change', this.onDataSourceChanged);
+
+		} catch (e) {
+			this.set('error', e);
+			this.emitChange('error', 'loading');
+		}
+	}
+
+	onDataSourceChanged = () => {
+		this.emitChange('items');
+	}
+}
--- src/sharing-lists/View.jsx
+++ src/sharing-lists/View.jsx
@@ -1,9 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Loading } from 'nti-web-commons';
 
-export default function SharingLists () {
-	return (
-		<div>
-			Sharing Lists
-		</div>
-	);
+import SharingListStore from './Store';
+import SharingListCard from './SharingListCard';
+
+const propMap = {
+	items: 'items'
+};
+
+export default
+@SharingListStore.connect(propMap)
+class SharingListsView extends React.Component {
+
+	state = {
+		showRenameDialog: false
+	}
+
+	static propTypes = {
+		store: PropTypes.object,
+		items: PropTypes.array
+	};
+
+	constructor (props) {
+		super();
+	}
+
+	// triggerRenameGroupModal = (props) => {
+	// 	console.log('Open rename group modal');
+	// 	console.log(props.entity.displayName);
+	// 	this.setState({ showRenameDialog: true });
+	// };
+
+	deleteSharingList = (props) => {
+		console.log ('deleting group');
+		console.log (props.entity.displayName);
+	};
+
+	renameSharingList = (props) => {
+		console.log ('view group code');
+		console.log (props.entity.displayName);
+	};
+
+	// componentDidMount () {
+	// 	console.log('component did mount');
+	// 	this.props.store.loadGroups();
+	// }
+
+	render () {
+
+		const {items, store} = this.props;
+
+		if (!store || store.loading) {
+			return <Loading.Mask />;
+		}
+
+		return (
+			<div className="sharing-lists-panel">
+				<h2 className="sharing-lists-panel-header">Sharing Lists</h2>
+				<div className="sharing-lists-list-frame">
+					{items && items.map(
+						(i) => (
+							<SharingListCard entity={i}
+								members={i.friends}
+								key={i.Username}
+								deleteSharingList={this.deleteSharingList}
+								renameSharingList={this.renameSharingList}/>
+						)
+					)}
+					{/* {this.state.showRenameDialog && (
+						<GroupActionModal/>
+					)} */}
+				</div>
+			</div>
+		);
+	}
 }
