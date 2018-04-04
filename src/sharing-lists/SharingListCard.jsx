21643707a9774e0e16fd6010a1c1ef9cb17f1044
Zachary Roux <zachary.roux@nextthought.com>
2018-04-04 13:51:56 -0500

    Add flyout options and placeholders for callbacks for sharing list view

--- src/sharing-lists/SharingListCard.jsx
+++ src/sharing-lists/SharingListCard.jsx
@@ -8,9 +8,22 @@ SharingListCard.propTypes = {
 		PropTypes.object,
 		PropTypes.string
 	]).isRequired,
-	members: PropTypes.array
+	members: PropTypes.array,
+	renameSharingList: PropTypes.func,
+	managePeople: PropTypes.func,
+	deleteSharingList: PropTypes.func
 };
 
-export default function SharingListCard ({entity, members}) {
-	return (<CardDetail entity={entity} members={members}/>);
+export default function SharingListCard ({entity, members, renameSharingList, managePeople, deleteSharingList}) {
+
+	const flyoutOptions = [
+		{className: 'sharing-list-action-flyout-option', displayText: 'Change Name', onClick: renameSharingList},
+		{className: 'sharing-list-action-flyout-option', displayText: 'Manage People', onClick: managePeople},
+		{className: 'sharing-list-action-flyout-option-delete', displayText: 'Delete List', onClick: deleteSharingList}
+	];
+
+	return (
+		<div className="sharing-list-card">
+			<CardDetail entity={entity} members={members} flyoutOptions={flyoutOptions}/>
+		</div>);
 }
