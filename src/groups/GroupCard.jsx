b683404e78a70d3f6cf0c9ec35fa63daf94310e0
Zachary Roux <zachary.roux@nextthought.com>
2018-04-18 09:52:02 -0500

    Add callback for leaveGroup, and conditionally add flyout options based on the links available for a group.

--- src/groups/GroupCard.jsx
+++ src/groups/GroupCard.jsx
@@ -11,17 +11,30 @@ GroupCard.propTypes = {
 	]).isRequired,
 	members: PropTypes.array,
 	deleteGroup: PropTypes.func,
+	leaveGroup: PropTypes.func,
 	renameGroup: PropTypes.func,
 	viewGroupCode: PropTypes.func
 };
 
-export default function GroupCard ({entity, members, deleteGroup, renameGroup, viewGroupCode}) {
+export default function GroupCard ({entity, members, deleteGroup, leaveGroup, renameGroup, viewGroupCode}) {
 
-	const groupFlyoutOptions = [
-		{className: 'group-action-flyout-option', displayText: 'View Group Code', onClick: viewGroupCode},
-		{className: 'group-action-flyout-option', displayText: 'Change Name', onClick: renameGroup},
-		{className: 'group-action-flyout-option-delete', displayText: 'Delete Group', onClick: deleteGroup}
-	];
+	const leaveGroupOption = {className: 'group-action-flyout-option-delete', displayText: 'Leave Group', onClick: leaveGroup};
+	const deleteGroupOption = {className: 'group-action-flyout-option-delete', displayText: 'Delete Group', onClick: deleteGroup};
+	const groupCodeOption = {className: 'group-action-flyout-option', displayText: 'View Group Code', onClick: viewGroupCode};
+	const changeNameOption = {className: 'group-action-flyout-option', displayText: 'Change Name', onClick: renameGroup};
+
+	// Make sure we only show the appropriate flyout options
+	// for whatever links we're getting back on the group.
+	let groupFlyoutOptions = [];
+	if (entity.hasLink('default-trivial-invitation-code')) {
+		groupFlyoutOptions.push(groupCodeOption);
+	}
+	if (entity.hasLink('edit')) {
+		groupFlyoutOptions.push(changeNameOption, deleteGroupOption);
+	}
+	if (entity.hasLink('my_membership')) {
+		groupFlyoutOptions.push(leaveGroupOption);
+	}
 
 	return (
 		<div className="group-card">
