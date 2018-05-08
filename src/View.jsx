f6dbddb9c9cc09edfd5b638eb458ff8ed12faaca
Zachary Roux <zachary.roux@nextthought.com>
2018-05-08 10:18:04 -0500

    Initial commit of a router view for switching between contacts, sharing lists, and groups pages.

---
+++ src/View.jsx
@@ -0,0 +1,11 @@
+import {Router, Route} from '@nti/web-routing';
+
+import GroupsView from './groups';
+import ContactListView from './contacts';
+import SharingListsView from './sharing-lists';
+
+export default Router.for([
+	Route({path: '/groups', component: GroupsView}),
+	Route({path: '/sharing-lists', component: SharingListsView}),
+	Route({path: '/', component: ContactListView}),
+], { title: 'Contacts'});
