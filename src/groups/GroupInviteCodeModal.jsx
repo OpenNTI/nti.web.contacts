6726f0ea32fd97ea814289256398d33eedbefc73
Zachary Roux <zachary.roux@nextthought.com>
2018-05-09 12:05:35 -0500

    Adjustments to invite code dialog (has known async errors for now though)

--- src/groups/GroupInviteCodeModal.jsx
+++ src/groups/GroupInviteCodeModal.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { scoped } from '@nti/lib-locale';
-import { Prompt, DialogButtons, Panels, Input } from '@nti/web-commons';
+import { Prompt, Panels, Input } from '@nti/web-commons';
+
+const t = scoped('nti-web-contacts.groups.GroupInviteCodeModal', {
+	headerText: 'Invite People',
+	bodyText: 'Share this group code to others you want to join your group. Once they click "Join a Group" they will paste in this code to join.'
+});
 
 export default class GroupInviteCodeModal extends React.Component {
 
 	static propTypes = {
-		onDismiss: PropTypes.func,
-		inviteCode: PropTypes.string
+		item: PropTypes.object,
+		onDismiss: PropTypes.func
 	};
 
 	onDismiss = () => {
 		this.props.onDismiss('showInviteCodeDialog');
 	}
 
+	getInviteCode = async () => {
+		const {item} = this.props;
+
+		const link = await item.fetchLink('default-trivial-invitation-code');
+		return link.invitation_code;
+	};
+
 	render () {
-		const {inviteCode} = this.props;
+		let inviteCode = this.getInviteCode();
+		// console.log(inviteCode);
 		return(
 			<Prompt.Dialog closeOnMaskClick onBeforeDismiss={this.onDismiss} title="Test">
 				<div className="group-action-modal">
 					<Panels.Header className="group-action-modal-header" onClose={this.onDismiss}>
-						Invite People
+						{t('headerText')}
 					</Panels.Header>
 					<div className="group-action-modal-content">
-						Share this group code to others you want to join your group. Once they click &quot;Join a Group&quot; they will paste in this code to join.
+						{t('bodyText')}
 						<div className="group-action-modal-content sub-header">Group Code</div>
 						{inviteCode && <Input.Text value={inviteCode}/>}
 					</div>
