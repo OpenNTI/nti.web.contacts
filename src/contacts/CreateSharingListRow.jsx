622b3df1dc2969e8629f9605ad68f976bc58382e
Zachary Roux <zachary.roux@nextthought.com>
2018-05-09 07:30:39 -0500

    Add a component so that we can create a new sharing list with the current contact while adding that contact to sharing lists. Needs some styling.

---
+++ src/contacts/CreateSharingListRow.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import EditableTextField from '../commons/EditableTextField';
+
+export default class CreateSharingListRow extends React.Component {
+
+	static propTypes = {
+		onFinish: PropTypes.func
+	};
+
+	state = {
+		inEditMode: false
+	}
+
+	onClickRow = () => {
+		this.setState({inEditMode: true});
+	}
+
+	onFinishedEditing = (name) => {
+		const {onFinish} = this.props;
+		this.setState({inEditMode: false});
+		onFinish(name);
+	}
+
+	render () {
+		return (
+			<div className="sharing-list-row" onClick={this.onClickRow}>
+				<EditableTextField text="Create new list"
+					isEditable={this.state.inEditMode}
+					onFinishedEditing={this.onFinishedEditing}/>
+			</div>
+		);
+	}
+}
