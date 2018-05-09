81d8fbc0a1164bdbbebb8e920e21408dc864bdc9
Zachary Roux <zachary.roux@nextthought.com>
2018-05-09 12:04:28 -0500

    Slight styling changes on contacts page (more work needed for sidebar)

---
+++ src/contacts/ContactSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import { scoped } from '@nti/lib-locale';
+
+export default class ContactSidebar extends React.Component {
+
+	render () {
+		return (
+			<div className="contact-sidebar">
+				My Contacts
+			</div>
+		);
+	}
+
+}
