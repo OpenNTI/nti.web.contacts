c60df953eb8152e62ece80700edc8017d2013515
Zachary Roux <zachary.roux@nextthought.com>
2018-05-09 12:06:41 -0500

    update index files, test harness js

--- test/app/index.js
+++ test/app/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
 
-import { ContactCard, ContactList, RouterView } from '../../src';
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import {Router, Route, LinkTo} from '@nti/web-routing';
+
+import RouterView from '../../src';
 import { CardDetail, Member, Members } from '../../src/commons';
 import { SharingListCard } from '../../src/sharing-lists';
 import SharingListsView from '../../src/sharing-lists/View';
@@ -12,8 +16,6 @@ import ContactListView from '../../src/contacts/View';
 
 import '@nti/style-common/all.scss';
 import '@nti/web-commons/lib/index.css';
-
-window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
 
 const RESOLVE = 'RESOLVE';
 const SEARCH = 'SEARCH';
@@ -27,191 +29,212 @@ const GROUPVIEW = 'GROUPVIEW';
 const CONTACTLISTVIEW = 'CONTACTLISTVIEW';
 const ROUTERVIEW = 'ROUTERVIEW';
 
-class TestKitchenSink extends React.Component {
+// class TestKitchenSink extends React.Component {
+//
+// 	state = {}
+//
+// 	renderMap = {
+// 		RESOLVE: 'renderResolve',
+// 		SEARCH: 'renderSearch',
+// 		CARD: 'renderCardDetail',
+// 		MEMBER: 'renderMember',
+// 		MEMBERS: 'renderMembers',
+// 		SHARINGLIST: 'renderSharingList',
+// 		SHARINGLISTVIEW: 'renderSharingListView',
+// 		GROUPCARD: 'renderGroupCard',
+// 		GROUPVIEW: 'renderGroupView',
+// 		CONTACTLISTVIEW: 'renderContactListView',
+// 		ROUTERVIEW: 'renderRouterView'
+// 	};
+//
+// 	constructor (props) {
+// 		super(props);
+// 		this.state = {
+// 			selectedOption: RESOLVE
+// 		};
+// 	}
+//
+// 	handleChange = (event) => {
+// 		this.setState({selectedOption: event.target.value});
+// 	}
+//
+//
+// 	renderMember () {
+// 		return (
+// 			<Member entity={{Username: 'Test user', displayName: 'Test user', initials: 'TU'}}/>
+// 		);
+// 	}
+//
+// 	renderMembers () {
+//
+// 		let members = [
+// 			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
+// 			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
+// 		];
+//
+// 		return (
+// 			<Members members={members}/>
+// 		);
+// 	}
+//
+// 	renderCardDetail () {
+//
+// 		let members = [
+// 			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
+// 			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
+// 		];
+//
+// 		return (
+// 			<CardDetail entity={{Username: 'Test card detail', displayName: 'Card Detail', initials: 'CD'}} members={members}/>
+// 		);
+// 	}
+//
+// 	renderSharingList () {
+//
+// 		let members = [
+// 			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
+// 			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
+// 		];
+//
+// 		return (
+// 			<SharingListCard entity={{Username: 'sharing_scope', displayName: 'Sharing List component', initials: 'SL'}} members={members}/>
+// 		);
+// 	}
+//
+// 	renderSharingListView () {
+// 		return (
+// 			<SharingListsView/>
+// 		);
+// 	}
+//
+// 	// renderGroupCard () {
+// 	//
+// 	// 	let members = [
+// 	// 		{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
+// 	// 		{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
+// 	// 	];
+// 	// 	let entity = {Username: 'sharing_scope', displayName: 'Group Card component', initials: 'GC'};
+// 	//
+// 	// 	return (
+// 	// 		<GroupCard entity={entity} members={members}/>
+// 	// 	);
+// 	// }
+//
+// 	renderGroupView () {
+// 		return (
+// 			<GroupsView/>
+// 		);
+// 	}
+//
+// 	renderContactListView () {
+// 		return (
+// 			<ContactListView/>
+// 		);
+// 	}
+//
+// 	renderRouterView () {
+// 		return ( <RouterView/> );
+// 	}
+//
+// 	render () {
+// 		// const selected = this.state.selectedOption;
+// 		// const form = (
+// 		// 	<form>
+// 		// 		<label>
+// 		// 			<input type="radio" value={RESOLVE} onChange={this.handleChange} checked={selected === RESOLVE} />
+// 		// 			Resolve User
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={SEARCH} onChange={this.handleChange} checked={selected === SEARCH}/>
+// 		// 			Search
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={CARD} onChange={this.handleChange} checked={selected === CARD}/>
+// 		// 			Card
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={MEMBER} onChange={this.handleChange} checked={selected === MEMBER}/>
+// 		// 			Member
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={MEMBERS} onChange={this.handleChange} checked={selected === MEMBERS}/>
+// 		// 			Members
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={SHARINGLIST} onChange={this.handleChange} checked={selected === SHARINGLIST}/>
+// 		// 			Sharing list Card
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={SHARINGLISTVIEW} onChange={this.handleChange} checked={selected === SHARINGLISTVIEW}/>
+// 		// 			Sharing list view
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={GROUPVIEW} onChange={this.handleChange} checked={selected === GROUPVIEW}/>
+// 		// 			Groups View
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={CONTACTLISTVIEW} onChange={this.handleChange} checked={selected === CONTACTLISTVIEW}/>
+// 		// 			Contact list view
+// 		// 		</label>
+// 		// 		<label>
+// 		// 			<input type="radio" value={ROUTERVIEW} onChange={this.handleChange} checked={selected === ROUTERVIEW}/>
+// 		// 			Router view
+// 		// 		</label>
+// 		// 	</form>
+// 		// );
+// 		//
+// 		// const bodyRenderer = this[this.renderMap[selected]];
+//
+// 		return (
+// 			<div>
+//
+// 				<RouterView/>
+//
+// 				{/* {form} */}
+// 				{/* {bodyRenderer()} */}
+// 			</div>
+// 		);
+// 	}
+// }
 
-	state = {}
+class Root extends React.Component {
 
-	renderMap = {
-		RESOLVE: 'renderResolve',
-		SEARCH: 'renderSearch',
-		CARD: 'renderCardDetail',
-		MEMBER: 'renderMember',
-		MEMBERS: 'renderMembers',
-		SHARINGLIST: 'renderSharingList',
-		SHARINGLISTVIEW: 'renderSharingListView',
-		GROUPCARD: 'renderGroupCard',
-		GROUPVIEW: 'renderGroupView',
-		CONTACTLISTVIEW: 'renderContactListView',
-		ROUTERVIEW: 'renderRouterView'
-	};
+	static contextTypes = {
+		router: PropTypes.object
+	}
 
-	constructor (props) {
-		super(props);
-		this.state = {
-			selectedOption: RESOLVE
+	static childContextTypes = {
+		router: PropTypes.object
+	}
+
+	getChildContext () {
+		return {
+			router: {
+				...this.context.router,
+				getRouteFor: () => {
+					return {
+						href: '/foo'
+					};
+				}
+			}
 		};
-	}
-
-	handleChange = (event) => {
-		this.setState({selectedOption: event.target.value});
-	}
-
-	renderResolve () {
-		const provider = () => {
-			return 'subtitle placeholder';
-		};
-
-		return (
-			<ContactCard entity={{Username: 'Test user', displayName: 'Test user', initials: 'TU'}} subtitleProvider={provider}/>
-		);
-	}
-
-	renderSearch () {
-		return (
-			<ContactList/>
-		);
-	}
-
-	renderMember () {
-		return (
-			<Member entity={{Username: 'Test user', displayName: 'Test user', initials: 'TU'}}/>
-		);
-	}
-
-	renderMembers () {
-
-		let members = [
-			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
-			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
-		];
-
-		return (
-			<Members members={members}/>
-		);
-	}
-
-	renderCardDetail () {
-
-		let members = [
-			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
-			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
-		];
-
-		return (
-			<CardDetail entity={{Username: 'Test card detail', displayName: 'Card Detail', initials: 'CD'}} members={members}/>
-		);
-	}
-
-	renderSharingList () {
-
-		let members = [
-			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
-			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
-		];
-
-		return (
-			<SharingListCard entity={{Username: 'sharing_scope', displayName: 'Sharing List component', initials: 'SL'}} members={members}/>
-		);
-	}
-
-	renderSharingListView () {
-		return (
-			<SharingListsView/>
-		);
-	}
-
-	renderGroupCard () {
-
-		let members = [
-			{Username: 'Test user', displayName: 'Test user', initials: 'TU'},
-			{Username: 'zroux', displayName: 'Zachary Roux', initials: 'ZR'}
-		];
-		let entity = {Username: 'sharing_scope', displayName: 'Group Card component', initials: 'GC'};
-
-		return (
-			<GroupCard entity={entity} members={members}/>
-		);
-	}
-
-	renderGroupView () {
-		return (
-			<GroupsView/>
-		);
-	}
-
-	renderContactListView () {
-		return (
-			<ContactListView/>
-		);
-	}
-
-	renderRouterView () {
-		return ( <RouterView/> );
 	}
 
 	render () {
-		const selected = this.state.selectedOption;
-		const form = (
-			<form>
-				<label>
-					<input type="radio" value={RESOLVE} onChange={this.handleChange} checked={selected === RESOLVE} />
-					Resolve User
-				</label>
-				<label>
-					<input type="radio" value={SEARCH} onChange={this.handleChange} checked={selected === SEARCH}/>
-					Search
-				</label>
-				<label>
-					<input type="radio" value={CARD} onChange={this.handleChange} checked={selected === CARD}/>
-					Card
-				</label>
-				<label>
-					<input type="radio" value={MEMBER} onChange={this.handleChange} checked={selected === MEMBER}/>
-					Member
-				</label>
-				<label>
-					<input type="radio" value={MEMBERS} onChange={this.handleChange} checked={selected === MEMBERS}/>
-					Members
-				</label>
-				<label>
-					<input type="radio" value={SHARINGLIST} onChange={this.handleChange} checked={selected === SHARINGLIST}/>
-					Sharing list Card
-				</label>
-				<label>
-					<input type="radio" value={SHARINGLISTVIEW} onChange={this.handleChange} checked={selected === SHARINGLISTVIEW}/>
-					Sharing list view
-				</label>
-				<label>
-					<input type="radio" value={GROUPCARD} onChange={this.handleChange} checked={selected === GROUPCARD}/>
-					Group card
-				</label>
-				<label>
-					<input type="radio" value={GROUPVIEW} onChange={this.handleChange} checked={selected === GROUPVIEW}/>
-					Groups View
-				</label>
-				<label>
-					<input type="radio" value={CONTACTLISTVIEW} onChange={this.handleChange} checked={selected === CONTACTLISTVIEW}/>
-					Contact list view
-				</label>
-				<label>
-					<input type="radio" value={ROUTERVIEW} onChange={this.handleChange} checked={selected === ROUTERVIEW}/>
-					Router view
-				</label>
-			</form>
-		);
-
-		const bodyRenderer = this[this.renderMap[selected]];
-
 		return (
 			<div>
-				{form}
-				{bodyRenderer()}
+				<LinkTo.Path to="/">Contacts component</LinkTo.Path> | <LinkTo.Path to="/groups/">Groups component</LinkTo.Path> | <LinkTo.Path to="/sharing-lists/">Sharing lists component</LinkTo.Path>
+				<RouterView/>
 			</div>
 		);
 	}
 }
 
+const TestRouter = Router.for([
+	Route({path: '/', component: Root, name: 'root'})
+], null, 'root');
+
 ReactDOM.render(
-	React.createElement(TestKitchenSink, {}),
+	React.createElement(TestRouter, {basename: '/'}),
 	document.getElementById('content')
 );
