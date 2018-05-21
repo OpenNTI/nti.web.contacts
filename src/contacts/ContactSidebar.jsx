117294ca8c081b780f7414aabc9aa5569dac0768
Zachary Roux <zachary.roux@nextthought.com>
2018-05-21 15:10:15 -0500

    Some preliminary work on sidebar router for contacts page.

--- src/contacts/ContactSidebar.jsx
+++ src/contacts/ContactSidebar.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { scoped } from '@nti/lib-locale';
+import PropTypes from 'prop-types';
+import { scoped } from '@nti/lib-locale';
+import {LinkTo} from '@nti/web-routing';
 
-export default class ContactSidebar extends React.Component {
+const t = scoped('nti-web-contacts.contacts.ContactSidebar', {
+	myContacts: 'My Contacts'
+});
 
-	render () {
-		return (
-			<div className="contact-sidebar">
-				My Contacts
-			</div>
-		);
-	}
+ContactSidebar.propTypes = {
+	classes: PropTypes.array
+};
 
+export default function ContactSidebar ({classes}) {
+	return (
+		<ul className="contacts-tabs">
+
+			<li className="sidebar-header">
+				<div className="sidebar-title">
+					Contacts
+				</div>
+			</li>
+
+			<li>
+				<LinkTo.Path to="./" activeClassName="active" exact>{t('myContacts')}</LinkTo.Path>
+			</li>
+			{/* <li>
+				<LinkTo.Path to="./courses" activeClassName="active">{t('courses')}</LinkTo.Path>
+			</li>
+			{
+				user.hasLink('transcript') && (
+					<li>
+						<LinkTo.Path to="./transcript" activeClassName="active">{t('transcript')}</LinkTo.Path>
+					</li>
+				)
+			}
+			<li>
+				<LinkTo.Path to="./reports" activeClassName="active">{t('reports')}</LinkTo.Path>
+			</li> */}
+		</ul>
+	);
 }
