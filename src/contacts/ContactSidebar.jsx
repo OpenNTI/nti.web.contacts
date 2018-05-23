e334ecbd0bd607708afa29b82946519965b5575e
Zachary Roux <zachary.roux@nextthought.com>
2018-05-23 16:08:38 -0500

    Some improved styling on the contacts page so that it's closer to the design. Also using NavContent container from Layouts.

--- src/contacts/ContactSidebar.jsx
+++ src/contacts/ContactSidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { scoped } from '@nti/lib-locale';
+import { Layouts } from '@nti/web-commons';
 import {LinkTo} from '@nti/web-routing';
 
 const t = scoped('nti-web-contacts.contacts.ContactSidebar', {
@@ -13,30 +14,20 @@ ContactSidebar.propTypes = {
 
 export default function ContactSidebar ({classes}) {
 	return (
-		<ul className="contacts-tabs">
 
-			<li className="sidebar-header">
-				<div className="sidebar-title">
-					Contacts
-				</div>
-			</li>
-
-			<li>
-				<LinkTo.Path to="./" activeClassName="active" exact>{t('myContacts')}</LinkTo.Path>
-			</li>
-			{/* <li>
-				<LinkTo.Path to="./courses" activeClassName="active">{t('courses')}</LinkTo.Path>
-			</li>
-			{
-				user.hasLink('transcript') && (
-					<li>
-						<LinkTo.Path to="./transcript" activeClassName="active">{t('transcript')}</LinkTo.Path>
+		<Layouts.NavContent.Container>
+			<Layouts.NavContent.Nav className="nav-bar">
+				<ul className="contacts-tabs">
+					<li className="sidebar-header">
+						<div className="sidebar-title">
+							Contacts
+						</div>
 					</li>
-				)
-			}
-			<li>
-				<LinkTo.Path to="./reports" activeClassName="active">{t('reports')}</LinkTo.Path>
-			</li> */}
-		</ul>
+					<li>
+						<LinkTo.Path to="./contacts" activeClassName="active" exact>{t('myContacts')}</LinkTo.Path>
+					</li>
+				</ul>
+			</Layouts.NavContent.Nav>
+		</Layouts.NavContent.Container>
 	);
 }
