ab3f3bed91c9645cb6b67f1b1b9aed94881bd0d5
Andrew Ligon <andrew.ligon@nextthought.com>
2018-06-01 16:10:19 -0500

    Use the list header component

--- src/contacts/ContactSidebar.jsx
+++ src/contacts/ContactSidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { scoped } from '@nti/lib-locale';
 import {LinkTo} from '@nti/web-routing';
+import {ListHeader} from '@nti/web-commons';
 
 const t = scoped('nti-web-contacts.contacts.ContactSidebar', {
 	myContacts: 'My Contacts'
@@ -15,9 +16,9 @@ export default function ContactSidebar ({classes}) {
 	return (
 		<ul className="contacts-tabs">
 			<li className="sidebar-header">
-				<div className="sidebar-title">
+				<ListHeader className="sidebar-title">
 					Contacts
-				</div>
+				</ListHeader>
 			</li>
 			<li>
 				<LinkTo.Path to="./" activeClassName="active" exact>{t('myContacts')}</LinkTo.Path>
