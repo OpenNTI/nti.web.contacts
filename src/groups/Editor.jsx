637fad0fa37f8f3ecf06a76bd6e58381dc55a0f4
Ray Hatfield <ray.hatfield@nextthought.com>
2019-02-20 14:49:29 -0600

    NTI-7441 - choose appropriate text for edit vs create

--- src/groups/Editor.jsx
+++ src/groups/Editor.jsx
@@ -5,11 +5,22 @@ import {DialogButtons, Panels, Input} from '@nti/web-commons';
 
 import {Membership} from '../common';
 
-const t = scoped('nti-web-contacts.groups.GroupCreateModal', {
+const edit = {
 	cancelButton: 'Cancel',
-	createButton: 'Create',
-	createGroupHeader: 'Create a Group',
-	createGroupDescription: 'Groups are great places to collaborate on projects or to share and discuss common interests. Share photos, videos, files and websites with your peers.'
+	saveButton: 'Save',
+	header: 'Edit',
+	description: 'Groups are great places to collaborate on projects or to share and discuss common interests. Share photos, videos, files and websites with your peers.'
+};
+
+const create = {
+	...edit,
+	saveButton: 'Create',
+	header: 'Create a Group'
+};
+
+const troot = scoped('nti-web-contacts.groups.GroupEditor', {
+	create,
+	edit
 });
 
 export default class GroupCreateModal extends React.Component {
@@ -47,53 +58,41 @@ export default class GroupCreateModal extends React.Component {
 	// tracking solely for saving; no need to setState and trigger a re-render
 	onMembershipChange = members => this.members = members
 
-	renderControls = () => {
-
-		const {
-			props: {
-				onCancel,
-				group: {displayName: name = ''} = {}
-			},
-			state: {displayName = name}
-		} = this;
-
-		const disabled = !displayName.trim();
-
-		const buttons = [
-			{label: t('cancelButton'), onClick: onCancel},
-			{label: t('createButton'), onClick: this.onSave, disabled}
-		];
-
-		return (
-			<div className="group-action-modal-controls">
-				<DialogButtons buttons={buttons}/>
-			</div>
-		);
-	}
-
 	render () {
 		const {
 			props: {
+				onCancel,
 				group,
 				group: {displayName: name = ''} = {}
 			},
 			state: {displayName = name}
 		} = this;
 
+		const disabled = !displayName.trim();
+		const localeKey = group ? 'edit' : 'create';
+		const t = key => troot([localeKey, key]);
+
+		const buttons = [
+			{label: t('cancelButton'), onClick: onCancel},
+			{label: t('saveButton'), onClick: this.onSave, disabled}
+		];
+
 		return (
 			<div className="group-action-modal">
 				<Panels.Header className="group-action-modal-header" onClose={this.onDismiss}>
-					{t('createGroupHeader')}
+					{t('header')}
 				</Panels.Header>
 				<div className="group-action-modal-content">
-					{t('createGroupDescription')}
+					{t('description')}
 					<div className="group-action-modal-content sub-header">Group Name</div>
 					<div>
 						<Input.Text placeholder="Name" value={displayName} onChange={this.updateDisplayName} maxLength="80"/>
 					</div>
 					<Membership onChange={this.onMembershipChange} entity={group} />
 				</div>
-				{this.renderControls()}
+				<div className="group-action-modal-controls">
+					<DialogButtons buttons={buttons} />
+				</div>
 			</div>
 		);
 	}
